cdb85ef647ebbb436fd596789ba23a4e23db1488
sajjat-ahmed <ahm.sajjat@gmail.com>
2020-07-17 11:20:50 +0600

    latest update

--- assignment.js
+++ assignment.js
@@ -1,16 +1,43 @@
 // feetToMile
 function feetToMile(feet){
     var mile = feet * 0.000189394;
-    return mile; 
+    if(mile < 0){
+        return "distance cannot be negative";
+    }
+    return mile;
 }
-var result = feetToMile(10000);
+var result = feetToMile(1000);
 console.log(result);
 
+
 // woodCalculator
+function woodCalculator(chare, table, bed){
+    var chareCount = chare * 1;
+    var tableCount = table * 2;
+    var bedCount = bed * 5;
+    var totalWood = chareCount + tableCount + bedCount;
+    if(totalWood < 0){
+        return "wood cannot be negative";
+    }
+    return totalWood;
+}
+var woodResult = woodCalculator(2, 5, 2);
+console.log(woodResult);
 
 
 // brickCalculator
 
 
-// tinyfriend
-
+// tinyFriend
+function tinyFriend(numbers){
+    var largest = numbers[0];
+    for(var i = 0; i<numbers.length; i++){
+        var currentNumber = numbers[i];
+        if(currentNumber > largest){
+            largest = currentNumber;
+        }
+    }
+    return largest;
+}
+var largestNumber = tinyFriend([12,23,323,432]);
+consolee.log(largestNumber);
