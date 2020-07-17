b1b8f7bcf2a38e206fe30cadc574d5158d6eb849
sajjat-ahmed <ahm.sajjat@gmail.com>
2020-07-17 09:32:56 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,16 @@
+// feetToMile
+function feetToMile(feet){
+    var mile = feet * 0.000189394;
+    return mile; 
+}
+var result = feetToMile(10000);
+console.log(result);
+
+// woodCalculator
+
+
+// brickCalculator
+
+
+// tinyfriend
+
